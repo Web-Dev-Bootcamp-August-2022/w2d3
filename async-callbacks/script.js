4eca19729e16de13779b3b02c5e9417e1b2a35cb
Jan Redmann <janredmann1@gmail.com>
2022-08-17 11:46:01 +0200

    add async callbacks

--- async-callbacks/script.js
+++ async-callbacks/script.js
@@ -1,1 +1,34 @@
 // async and callbacks
+
+// setTimeout
+// function someCallback() {
+// 	console.log('Hi everybody 🙂')
+// }
+
+// const id = setTimeout(function () {
+// 	console.log('Hi everybody 🙂')
+// }, 2000)
+// console.log('this runs first ⏰')
+// you can cancel the triggered timeout
+// clearTimeout(id)
+
+let counter = 1
+let timeoutId
+function callback() {
+	console.log(counter)
+	timeoutId = setTimeout(callback, 1000)
+	counter++
+	if (counter > 3) {
+		clearTimeout(timeoutId)
+		console.log('done')
+	}
+}
+// callback()
+
+// a better way to build a counter
+// setInterval
+let i = 1
+setInterval(function () {
+	console.log(i)
+	i++
+}, 1000)
