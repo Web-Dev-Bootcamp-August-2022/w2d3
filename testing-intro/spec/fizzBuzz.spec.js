d29ac596256b08758f28ca9bd4c445b49413dfba
Jan Redmann <janredmann1@gmail.com>
2022-08-17 14:26:48 +0200

    add testing intro

--- testing-intro/spec/fizzBuzz.spec.js
+++ testing-intro/spec/fizzBuzz.spec.js
@@ -5,5 +5,34 @@
 // in all other cases you just say the number
 // write a function that gets a number as a parameter and returns the correct result
 
-const fizzBuzz = require('../src/fizzBuzz')
+const fizzbuzz = require('../src/fizzBuzz')
+describe('fizzbuzz', function () {
+	it('is a function', function () {
+		expect(typeof fizzbuzz).toEqual('function');
+	});
+	it('returns 1 for 1', function () {
+		expect(fizzbuzz(1)).toEqual(1);
+	});
+	it('returns 2 for 2', function () {
+		expect(fizzbuzz(2)).toEqual(2);
+	});
+	it('returns fizz for 3', function () {
+		expect(fizzbuzz(3)).toEqual('fizz');
+	});
+	it('returns fizz for 6', function () {
+		expect(fizzbuzz(6)).toEqual('fizz');
+	});
+	it('returns buzz for 5', function () {
+		expect(fizzbuzz(5)).toEqual('buzz');
+	});
+	it('returns buzz for 10', function () {
+		expect(fizzbuzz(10)).toEqual('buzz');
+	});
+	it('returns fizzbuzz for 15', function () {
+		expect(fizzbuzz(15)).toEqual('fizzbuzz');
+	});
+	it('returns fizzbuzz for 30', function () {
+		expect(fizzbuzz(30)).toEqual('fizzbuzz');
+	});
 
+})
--- testing-intro/src/fizzBuzz.js
+++ testing-intro/src/fizzBuzz.js
@@ -1,5 +1,20 @@
 //
-
+function fizzbuzz(num, string = '') {
+	if (num % 15 === 0) {
+		return 'fizzbuzz'
+	}
+	if (num % 3 === 0) {
+		return 'fizz'
+	}
+	if (num % 5 === 0) {
+		return 'buzz'
+	}
+	// return num
+	// let string = ''
+	if (num % 3 === 0) string += 'fizz'
+	if (num % 5 === 0) string += 'buzz'
+	return string || num
+}
 
 if (typeof module !== undefined) {
 	module.exports = fizzbuzz;
