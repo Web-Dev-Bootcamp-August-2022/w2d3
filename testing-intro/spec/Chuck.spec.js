5b7cd488e624ae122e0342f44f942764cbdd3bcf
Jan Redmann <janredmann1@gmail.com>
2022-08-17 12:29:01 +0200

    add testing examples

--- testing-intro/spec/Chuck.spec.js
+++ testing-intro/spec/Chuck.spec.js
@@ -1,1 +1,30 @@
 const Chuck = require('../src/Chuck')
+
+describe('Chuck', function () {
+	let chuck;
+	const joke = 'Chuck Norris can divide by zero'
+
+	const mockMath = Object.create(global.Math)
+	mockMath.random = function () {
+		return 0.5
+	}
+	global.Math = mockMath
+	beforeEach(function () {
+		chuck = new Chuck()
+	})
+	describe('add joke function', function () {
+		it('should add a joke', function () {
+			// add a joke using addJoke()
+			chuck.addJoke(joke)
+			expect(chuck.jokes).toContain(joke)
+		})
+	})
+	describe('get random joke function', function () {
+		it('(Math.random) always returns 0.5 now', function () {
+			expect(Math.random()).toEqual(0.5)
+		})
+		it('should return a random joke', function () {
+			expect(chuck.getRandomJoke()).toEqual('Chuck Norris is the only man to ever defeat a brick wall in a game of tennis');
+		});
+	})
+})
--- testing-intro/spec/greeting.spec.js
+++ testing-intro/spec/greeting.spec.js
@@ -1,1 +1,15 @@
 // this is the test for the greeting function
+const greeting = require('../src/greeting')
+
+// test suite
+describe('The function greeting', function () {
+	// spec - specification
+	it('should be a function', function () {
+		// expectation using a matcher
+		expect(typeof greeting).toBe('function')
+	});
+	it('should return a greeting string', function () {
+		expect(greeting()).toEqual('hello ironhackers');
+	});
+})
+
